9251a24ed8cf9dcca1d89ed20087281a8b4f9c4d
kaylieko <kohy@oregonstate.edu>
2018-04-28 18:29:54 -0700

    Assignment 3 - supplyCount unittest

--- projects/kohy/dominion/unittest1.c
+++ projects/kohy/dominion/unittest1.c
@@ -5,20 +5,21 @@
  * unittest1: unittest1.c dominion.o rngs.o
  *      gcc -o unittest1 -g  unittest1.c dominion.o rngs.o $(CFLAGS)
  * ---------------------------------------------------------------------*/
-
 #include "dominion.h"
 #include "dominion_helpers.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 #include "rngs.h"
 
 
 void asserttrue (int test, int expect){
 	if (test == expect){
-		printf("Supply counted correctly. Test PASSED!\n\n");
+		printf("Test PASSED! Supply counted correctly.\n\n");
 	} 
 	else {
-		printf("Test FAILED!\n\n");
+		printf("Test FAILED! Supply counted incorrectly.\n\n");
 	}
 }
 
@@ -29,27 +30,22 @@ int main () {
 	int returnStatus;
 	int k[10] = {adventurer, smithy, council_room, village, great_hall, gardens, steward, feast, minion, embargo};
 
-	struct gameState G1;
-	struct gameState G2;
+	struct gameState G;
 
-	printf ("\n=====================================================\n");
-	printf ("	  UNITTEST #1 supplyCount():		\n");
-	printf ("=====================================================\n");
+	printf ("\n====================================================================\n");
+	printf ("	  	UNITTEST #1 supplyCount():		\n");
+	printf ("====================================================================\n");
 
-	printf("[Test1-1] Supply count of a Province card when 2 players play a game\n");
-	memset(&G1, 23, sizeof(struct gameState));		// clear the game state
-	initializeGame(numPlayer, k, seed, &G1);		// initialize a new game
-	G1.supplyCount[province] = 8;
-	returnStatus = 8;
-	asserttrue(supplyCount(province, &G1), returnStatus);
+	printf("\n[Test1-1] Supply count of a Province card when 2 players play a game\n");
+	memset(&G, 23, sizeof(struct gameState));		// clear the game state
+	initializeGame(numPlayer, k, seed, &G);			// initialize a new game
+	returnStatus = G.supplyCount[province];
+	asserttrue(supplyCount(province, &G), returnStatus);
 
 	printf("[Test1-2] Supply count of a Province card when 3 players play a game\n");
 	numPlayer = 3;
-	memset(&G2, 23, sizeof(struct gameState));		// clear the game state
-	initializeGame(numPlayer, k, seed, &G2);		// initialize a new game
-	G2.supplyCount[province] = 12;
-	returnStatus = 12;
-	asserttrue(supplyCount(province, &G2), returnStatus);
-
-  return 0;
+	initializeGame(numPlayer, k, seed, &G);		// initialize a new game
+	returnStatus = G.supplyCount[province];
+	asserttrue(supplyCount(province, &G), returnStatus);
+	return 0;
 }
