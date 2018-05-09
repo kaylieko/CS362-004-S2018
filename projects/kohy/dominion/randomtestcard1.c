0af105944e8b0f28da5aae984e517e0168e35596
kaylieko <kohy@oregonstate.edu>
2018-05-09 09:32:14 -0700

    Assignment 4 Great Hall Random Test

--- projects/kohy/dominion/randomtestcard1.c
+++ projects/kohy/dominion/randomtestcard1.c
@@ -13,9 +13,10 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 #define TESTCARD "great_hall"
-#define NUM_TESTS 1000
+#define NUM_TESTS 10
 
 void asserttrue (int test, int expect){
 	if (test == expect){
@@ -23,13 +24,18 @@ void asserttrue (int test, int expect){
 	} 
 	else {
 		printf("--> [Test FAILED!]\n");
+	
 	}
 }
 
 int main(){
+	int seed = 1000;
 	int thisPlayer = 0;
 	int handPos = 0;
-	int discardedCard = 1;		// 1 Great_hall card will be played
+	int choice1 = 0;
+	int choice2 = 0;
+	int choice3 = 0;
+	int playedCard = 1;		// 1 Great_hall card will be played
 	int drawnCard = 1;		// increase player's handsize by drawing 1 card
 
 	int k[10] = {adventurer, smithy, council_room, village, great_hall, gardens, steward, feast, minion, embargo};
@@ -38,9 +44,9 @@ int main(){
 
 	int i;
 	int numPlayers;
-	int seed;
-	int numDeck;
 	int expectedResult;
+	//int numDeckCards;
+	
 
 	printf ("\n=====================================================\n");
 	printf ("	  RANDOM CARD TEST 1 Great Hall		\n");
@@ -50,64 +56,52 @@ int main(){
 	for (i = 0; i < NUM_TESTS; i++){
 		//random numPlayers (min.2 to max.4)
 		numPlayers = rand() % 3 + 2;
-		//random seed (0 to 999)
-		seed = rand() % 1000; 
 
 		//initialize the game
 		initializeGame (numPlayers, k, seed, &G);
 
+		//random current player
 		thisPlayer = rand() % numPlayers;
-		G.deckCount[thisPlayer] = rand() % MAX_DECK;
+		//printf("%d\n", thisPlayer);
+
+		//random number of cards in deck
+		G.deckCount[thisPlayer] = rand() % (MAX_DECK - 3 + 1) + 3;
+
+		//random number of cards in hand
 		G.handCount[thisPlayer] = rand() % MAX_HAND;
 
-		//copy original gameState G to testG
-		memcpy(&test, &G, sizeof(struct gameState));
-		//decide the random current player
-		
-		
-		// add a great_hall card to current player's hand
-		//testG.hand[thisPlayer][testG.handCount[thisPlayer]] = great_hall;
-		//testG.handCount[thisPlayer]++;
-		//asserttrue(G.handCount[thisPlayer] + 1, testG.handCount[thisPlayer]);
+		//copy gameState
+		memcpy(&testG, &G, sizeof(struct gameState));
 
-		//call the card function to test
-		great_HallEffect(thisPlayer, &testG, handPos);
-	
-		
+		printf ("------------------------------------------------------------------------------------\n");
+		//printf("Number of Actions this player originally has: %i\n", G.numActions);
+		printf("Deck1: %i \n", G.deckCount[thisPlayer]);
+		//great_hall card effect	
+		cardEffect(great_hall, choice1, choice2, choice3, &testG, handPos, 0);
 
+		printf("Deck2: %i \n", testG.deckCount[thisPlayer]);
 		//check num of cards in hand, supposed to get +1(drawn)-1(played) after great_hall
-		expectedResult = G.handCount[thisPlayer] + drawnCard - discardedCard;
+		expectedResult = G.handCount[thisPlayer] + drawnCard - playedCard;
 		printf ("[Test1] Expected cards in HAND: %d vs. Tested cards in HAND: %d ", expectedResult, testG.handCount[thisPlayer]);
-		asserttest(testG.handCount[thisPlayer], expectedResult);
+		asserttrue(testG.handCount[thisPlayer], expectedResult);
 
 		//check num of cards in deck, supposed to have -1
 		//great_hall: player gets 1 card in hand from his deck
-		printf ("[Test2] Expected cards in DECK: %d vs. Tested cards in DECK: %d ", G.deckCount[thisPlayer] - 1, testG.deckCount[thisPlayer]);
-		asserttest(testG.handCount[thisPlayer], G.deckCount[thisPlayer] - 1);
+		expectedResult = G.deckCount[thisPlayer] - 1;
+		printf ("[Test2] Expected cards in DECK: %d vs. Tested cards in DECK: %d ", expectedResult, testG.deckCount[thisPlayer]);
+		asserttrue(testG.deckCount[thisPlayer], expectedResult);
 
 		//check num of actions, supposed to get +1 after great_hall
 		printf ("[Test3] Expected numActions: %d vs. Tested numActions: %d ", G.numActions + 1, testG.numActions);
 		asserttrue(testG.numActions, G.numActions+ 1);
 
-		//check num of actions, supposed to get +1 after great_hall
-		printf ("[Test4] Expected discard: %d vs. Tested discard: %d ", G.playedCardCount + discardedCard, testG.playedCardCount);
-		asserttrue(testG.playedCardCount, G.playedCardCount + discardedCard);
-	}
+		//check num of played card
+		printf ("[Test4] Expected playedCard: %d vs. Tested playedCard: %d ", G.playedCardCount + playedCard, testG.playedCardCount);
+		asserttrue(testG.playedCardCount, G.playedCardCount + playedCard);
 
+
+
+	}
 	return 0;
 }
-
-/* Great Hall in Dominion
-int great_HallEffect(int currentPlayer, struct gameState *state, int handPos){
-	//+1 Card
-  	drawCard(currentPlayer, state); 
-
-  	//+1 Actions
-  	//*Introduce a Bug: gain +2 actions instead of +1
-  	state->numActions = state->numActions + 2;     
-
-  	//discard card from hand
-  	discardCard(handPos, currentPlayer, state, 0);
-  	return 0;
-}*/
 
